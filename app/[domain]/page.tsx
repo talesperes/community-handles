c3cefad552acfb584827b6ce5b1f779dc0cd0077
Gurieel <guriel.dev@gmail.com>
2024-09-01 23:42:09 -0300

    Update page.tsx

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -110,7 +110,7 @@ export default async function IndexPage({
     <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-4">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          destaque-se no Bluesky
+          Destaque-se no Bluesky
           <br className="hidden sm:inline" />
           Comunidade {domain}
         </h1>
