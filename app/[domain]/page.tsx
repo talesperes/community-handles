1d7b3d04b68adf6fd603c546cb381e4377baf4d5
Gurieel <guriel.dev@gmail.com>
2024-09-01 23:44:22 -0300

    Update page.tsx

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -112,7 +112,7 @@ export default async function IndexPage({
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
           Destaque-se no Bluesky
           <br className="hidden sm:inline" />
-          Comunidade {domain}
+          Utilize {domain}
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
           Siga as instruções abaixo para mudar seu @ utilizando {domain}
