34b4af7baadaf101932636c079509e750d902236
Gurieel <guriel.dev@gmail.com>
2024-09-01 23:39:28 -0300

    update

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -110,8 +110,9 @@ export default async function IndexPage({
     <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-4">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Use {domain} <br className="hidden sm:inline" />e destaque-se no
-          Bluesky
+          destaque-se no Bluesky
+          <br className="hidden sm:inline" />
+          Comunidade {domain}
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
           Siga as instruções abaixo para mudar seu @ utilizando {domain}
